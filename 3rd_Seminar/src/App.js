30cf56b076dd568eeec7e807f75244de25d0b17b
Danna Lee <daeunlee0720@gmail.com>
2021-04-24 17:09:28 +0900

    useState, useEffect 활용 리팩토링

--- 3rd_Seminar/src/App.js
+++ 3rd_Seminar/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from './components/SearchBar';
 import UserCard from './components/UserCard';
 import { getUserData } from './lib/api';
 
 function App() {
 
-  const data = getUserData("Daeun-Danna-Lee");
+  const [userData, setUserData] = useState(null);
+
+  const getData = async () => {
+    const data = await getUserData("Daeun-Danna-Lee");
+    setUserData(data);
+  };
+
+  useEffect(() => {
+    getData();
+    console.log(userData);
+  },[]);
 
   return (
     <>
