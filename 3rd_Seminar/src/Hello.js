ff186dbc6680632dbb0f52abe8acb89ccd8adc0f
Danna Lee <daeunlee0720@gmail.com>
2021-04-24 16:36:01 +0900

    useEffect

--- 3rd_Seminar/src/Hello.js
+++ 3rd_Seminar/src/Hello.js
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
+import React, {useEffect} from "react";
+import reactDom from "react-dom";
 
 function Hello() {
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = React.useState(0);
+
+    React.useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    }, [count]);
+    // count 값이 변경될 때마다 새로 불러오게 됨
 
     return (
         <>
