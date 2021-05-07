6f2b0e6800fc67e9fefc769849319cd7264dd19a
Danna Lee <daeunlee0720@gmail.com>
2021-05-07 18:01:17 +0900

    [과제] step7 promise ui

--- 3rd_Seminar/src/lib/api.js
+++ 3rd_Seminar/src/lib/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const client = axios.create({
-//     baseURL: "https://api.github.com/users/",
-// });
+const client = axios.create({
+    baseURL: "https://api.github.com/users/",
+});
 
 // export const getUserData = async (name) => {
 //     try {
@@ -12,9 +12,20 @@ import axios from 'axios';
 // }
 
 // export는 따로 해줘도 되고, 함수 앞에 붙여줘도 됨
-export const getUserData = async (name) => { // 비동기 처리를 위한 async, await
-    // data는 객체로 받아오기 때문에 중괄호 안에 넣어줘야 함
-    const { data } = await axios.get("https://api.github.com/users/" + name);
-    // console.log(data);
-    return data;
-}
+// export const getUserData = async (name) => { // 비동기 처리를 위한 async, await
+//     // data는 객체로 받아오기 때문에 중괄호 안에 넣어줘야 함
+//     const { data } = await axios.get("https://api.github.com/users/" + name);
+//     // console.log(data);
+//     return data;
+// }
+
+export const getUserData = async (name) => {
+    try {
+      const { data } = await client.get(name);
+      console.log("[SUCCESS] GET user data", data);
+      return data;
+    } catch (e) {
+      console.log("[FAIL] GET user data", e);
+      return null;
+    }
+  };
